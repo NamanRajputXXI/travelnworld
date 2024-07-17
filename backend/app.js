e5c6d8571263dfd06ea1b1d39e41d529da027bfe
Aamir-khan19 <vintageaamir@example.com>
2024-07-17 14:44:46 +0530

    feat:create the server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+
+const app = express();
+
+// Load environment variables
+config({ path: "./config/config.env" });
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // To parse JSON request bodies
+
+// Routes
+app.get("/", (req, res) => {
+    console.log("server working");
+    res.send("Server is working");
+});
+
+export default app;
