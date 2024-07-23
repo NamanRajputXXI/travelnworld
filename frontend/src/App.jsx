8753c8b9c8ad1f685bf897d858f2d90ed977c486
Aamir-khan19 <vintageaamir@example.com>
2024-07-23 18:15:08 +0530

    ui:make the contact form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,31 @@
 import React from "react";
-// import Herosection from "./components/home/Herosection";
-// import Client from "./components/home/Client";
-// import Footer from "./components/global/Footer";
-// import Navbar from "./components/global/Navbar";
-// import GrowBuisness from "./components/home/GrowBuisness";
-// import WeddingCarouselTop from "./components/home/WeddingCarouselTop";
-// import AboutUsSectionHomepage from "./components/home/AboutUsSectionHomepage";
-// import OurTravelAgents from "./components/home/OurTravelAgents";
-// import InternationalTopTourPackage from "./components/home/InternationalTopTourPackage";
-// import DomesticTopTourPackage from "./components/home/DomesticTopTourPackage";
-// import VerifiedHotel from "./components/home/VerifiedHotel";
-// import VerifiedTransport from "./components/home/VerifiedTransport";
-// import FreeRegisteredTravelAgents from "./components/home/FreeRegisteredTravelAgents";
-// import BlogSection from "./components/home/BlogSection";
-// import Testimonial from "./components/home/Testimonial";
-// import About from "./Pages/About";
-// import TeamSection from "./components/about/TeamSection";
-// import PricingSection from "./components/about/PricingSection";
-// import OurClients from "./components/about/OurClients";
-import About from "./Pages/About.jsx"
+import Herosection from "./components/home/Herosection";
+import Client from "./components/home/Client";
+import Footer from "./components/global/Footer";
+import Navbar from "./components/global/Navbar";
+import GrowBuisness from "./components/home/GrowBuisness";
+import WeddingCarouselTop from "./components/home/WeddingCarouselTop";
+import OurTravelAgents from "./components/home/OurTravelAgents";
+import InternationalTopTourPackage from "./components/home/InternationalTopTourPackage";
+import DomesticTopTourPackage from "./components/home/DomesticTopTourPackage";
+import VerifiedHotel from "./components/home/VerifiedHotel";
+import VerifiedTransport from "./components/home/VerifiedTransport";
+import FreeRegisteredTravelAgents from "./components/home/FreeRegisteredTravelAgents";
+import BlogSection from "./components/home/BlogSection";
+import Testimonial from "./components/home/Testimonial";
+import About from "./Pages/About";
+import TeamSection from "./components/about/TeamSection";
+import PricingSection from "./components/about/PricingSection";
+import OurClients from "./components/about/OurClients";
 const App = () => {
   return (
     <>
 
-<About/>
 
-      {/* <Navbar /> */}
-      {/* <About/>
-    <TeamSection/>
-      <PricingSection/>
-      <OurClients/> */}
-      {/* <WeddingCarouselTop />
+
+       <Navbar /> 
+      <WeddingCarouselTop />
       <Herosection />
-      <AboutUsSectionHomepage />
       <Client />
       <OurTravelAgents />
       <InternationalTopTourPackage />
@@ -46,7 +39,7 @@ const App = () => {
       <GrowBuisness />
       <BlogSection />
       <Testimonial />
-      <Footer /> */}
+      <Footer />
     </>
   );
 };
